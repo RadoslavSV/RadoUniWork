23a8234257542e45382c15821ed0fccebe062ace
RadoslavSV <djradi10@abv.bg>
2021-01-01 23:47:34 +0200

    added function that counts the number of digits in a number as well as started writing the biggest/main function that will calculate the expression

--- 62528_calculator_UP_proekt.cpp
+++ 62528_calculator_UP_proekt.cpp
@@ -17,6 +17,7 @@
 
 #include <iostream>
 #include <fstream>             //use <fstream> library in order to be able to read and write in files
+#include <cmath>
 using namespace std;
 
 int LengthOfString(char s[])                //implement a function that counts how many symbols are there in the string
@@ -60,6 +61,9 @@ bool CorrectSequenceOfSymbols(char str[])        //write a function that will ch
             }
             i=m;   //by doing this, it's like we just skip all digits that are next to one another (obviously, because they compose a multi-digit number)
 
+            if(str[i]=='+' || str[i]=='-' || str[i]=='*' || str[i]=='/' || str[i]=='*') i--;
+            //in the case where the operation is right after the number, that index should be decreased once, in order not to count the symbol for operation as the last digit of the number
+
             int j;
             for(j=i+1;str[j]==' ';j++);     //when a symbol different than an interval is encountered:
             if(str[j]>='0' && str[j]<='9') return false;     //if that symbol is again a digit, this means the read data is unallowed
@@ -82,12 +86,119 @@ bool CorrectSequenceOfSymbols(char str[])        //write a function that will ch
     return true;   //eventually, if nothing illegal is found, return true (thus the character sequence can be calculated)
 }
 
+int LengthOfNumber(double num)     //write a function that returns the number of digits in a number (both integer and double)
+{
+    int count = 0;               //set counter to zero
+    if((int)num==0) count++;     //if the number is 0./something/, we add one for that 0
+    double a=num;                //define a second main variable from type double that we use later
+    while(int(num)!=0)           //convert the real variable to an integer (thus use only its digits before the decimal point)
+    {
+        num /= 10;              //remove the last digits each iteration
+        count++;                //increase the counter
+    }
+    a -= int(a);                //after the integer counting is complete, we remove the whole integer part of the whole number and continue (if the whole number is integer, nothing else happens)
+    while (a != 0)              //while it has not come to full zero
+    {
+        a = a * 10;             //move the decimal point once to the right
+        count++;
+        a = a - int(a);         //and again (each iteration) remove the newly-formed integer part
+    }
+    if(count>48) return 3;      //if the decimal is endless, it has three digits (a.bc or ab.c)
+    else return count;          //in the general case, return the counter
+}
+
+void CalculateTheExpression(char str[])
+{
+    for(int i=0;i<LengthOfString(str);i++)
+    {
+        if(str[i]=='*' || str[i]=='/' || str[i]=='^')
+        {
+            int j;
+            for(j=i+1;str[j]==' ';j++);
+
+            int m=j;
+            if(str[j+1]>='0' && str[j+1]<='9')
+            {
+                for(m=j+1;str[m]>='0' && str[m]<='9';m++);
+            }
+            else m=j+1;
+
+            int rightBorder=m-1;
+
+            long long actualRightNumber=0;
+            int currentDigit=0;
+            int multiplier=1;
+            do
+            {
+                currentDigit = str[m-1] - '0';
+                actualRightNumber += currentDigit * multiplier;
+                multiplier *= 10;
+                m--;
+
+            }while(m>j);
+
+
+            int l;
+            for(l=i-1;str[l]==' ';l--);
+
+            int n=l;
+            if(str[l-1]>='0' && str[l-1]<='9')
+            {
+                for(n=l-1;str[n]>='0' && str[n]<='9';n--);
+            }
+            else n=l-1;
+
+            int leftBorder=n+1;
+
+            int lengthOfNumber=l-n;
+
+
+            long long actualLeftNumber=0;
+            int currentDigitLeft=0;
+            int multiplierLeft=1;
+            do
+            {
+                currentDigitLeft = str[l] - '0';
+                actualLeftNumber += currentDigitLeft * multiplierLeft;
+                multiplierLeft *= 10;
+                l--;
+
+            }while(l>n);
+
+            if(str[i]=='*')
+            {
+                double result=0;
+                result = (double)actualLeftNumber*(double)actualRightNumber;
+
+                cout<<LengthOfNumber(result)<<endl;
+
+            }
+            if(str[i]=='/')
+            {
+                double result=0;
+                result = (double)actualLeftNumber/(double)actualRightNumber;
+                cout<<LengthOfNumber(result)<<endl;
+
+            }
+            if(str[i]=='^')
+            {
+                double result=0;
+                result = pow((double)actualLeftNumber,(double)actualRightNumber);
+                cout<<LengthOfNumber(result)<<endl;
+
+            }
+
+        }
+    }
+//cout<<str;
+}
+
 int main()
 {
     const int MAX_SIZE=10000;      //initialize a big enough constant variable that will serve as the maximum size of the read string from the file
     char str[MAX_SIZE];            //define a character sequence that will be read from the .txt file
     ifstream arithmeticalExpression;         //input stream class to operate on files - "arithmeticalExpression" so we can read the written arithmetical expression from a file
-    arithmeticalExpression.open("test2.txt",ios::in);         ////open the .txt file
+    arithmeticalExpression.open("test2.txt",ios::in);         //open the .txt file
     if(!arithmeticalExpression)               //initially, check if the program was able to open the file
     {
         cout<<"NaN"<<endl;                    //if not - then print appropriate message for an error and do not continue below
@@ -98,10 +209,10 @@ int main()
         if(not CorrectInputData(str)) cout<<"NaN"<<endl;       //firstly, use the aforeimplemented function to check for illegal symbols
         else      //if no such symbols are found:
         {
-            if(not CorrectSequenceOfSymbols(str)) cout<<"NaN"<<endl;
+            if(not CorrectSequenceOfSymbols(str)) cout<<"NaN"<<endl;   //secondly, use the function that checks if the order of the read symbols is possible
             else
             {
-                cout<<"sure"<<endl;
+                CalculateTheExpression(str);
             }
         }
 
